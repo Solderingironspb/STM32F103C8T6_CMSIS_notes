accf5ef5d2766e2c4a3050cabdd0df1cacd11fca
Solderingironspb <68805120+Solderingironspb@users.noreply.github.com>
2022-08-06 00:52:44 +0300

    Lesson_2

---
+++ CMSIS_Lesson2_EXTI/Core/Inc/main.h
@@ -0,0 +1,24 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32f1xx.h"
+	
+void CMSIS_Debug_init(void); //Настройка Debug (Serial Wire)
+void CMSIS_RCC_SystemClock_72MHz(void); //Настрока тактирования микроконтроллера на частоту 72MHz
+void CMSIS_SysTick_Timer_init(void); //Инициализация системного таймера
+void Delay_ms(uint32_t Milliseconds); //Функция задержки
+void SysTick_Handler(void); //Прерывания от системного таймера
+void CMSIS_PC13_OUTPUT_Push_Pull_init(void); //Пример настройки ножки PC13 в режим Push-Pull 50 MHz
+void CMSIS_PA8_MCO_init(void); //Пример настройки ножки PA8 в выход тактирующего сигнала c MCO
+	
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
