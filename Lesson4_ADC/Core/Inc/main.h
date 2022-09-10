768a45750538d0451b53aa854c3a7cb0b3fda97b
Solderingironspb <68805120+Solderingironspb@users.noreply.github.com>
2022-09-10 17:33:02 +0300

    Lesson_4 add

---
+++ Lesson4_ADC/Core/Inc/main.h
@@ -0,0 +1,17 @@
+
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	
+#include "stm32f1xx.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
