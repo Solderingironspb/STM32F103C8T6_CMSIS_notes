55d654663bb089ba08454c96bf783233144364ac
Олег Волков <68805120+Solderingironspb@users.noreply.github.com>
2023-07-16 02:44:30 +0300

    Update stm32f103xx_CMSIS.h
    CMSIS_GPIO_init() +

--- stm32f103xx_CMSIS.h
+++ stm32f103xx_CMSIS.h
@@ -38,6 +38,30 @@ extern "C" {
         uint16_t rx_counter; //Счетчик приходящих данных типа uint8_t по USART
         uint16_t rx_len; //Количество принятых байт после сработки флага IDLE
     };
+	
+	//GPIO Configuration mode
+	enum { 
+		GPIO_GENERAL_PURPOSE_OUTPUT,
+		GPIO_ALTERNATIVE_FUNCTION_OUTPUT,
+		GPIO_INPUT
+	};
+	//GPIO Input/OUTPUT type
+	enum { 
+		GPIO_OUTPUT_PUSH_PULL,
+		GPIO_OUTPUT_OPEN_DRAIN,
+		GPIO_INPUT_ANALOG,
+		GPIO_INPUT_FLOATING,
+		GPIO_INPUT_PULL_DOWN,
+		GPIO_INPUT_PULL_UP
+	};
+	
+	//GPIO Maximum output speed
+	enum {
+		GPIO_SPEED_RESERVED,
+		GPIO_SPEED_10_MHZ,
+		GPIO_SPEED_2_MHZ,
+		GPIO_SPEED_50_MHZ 
+	};
 
     void CMSIS_Debug_init(void); //Настройка Debug (Serial Wire)
     void CMSIS_RCC_SystemClock_72MHz(void); //Настрока тактирования микроконтроллера на частоту 72MHz
@@ -45,6 +69,7 @@ extern "C" {
     void Delay_ms(uint32_t Milliseconds); //Функция задержки
     void SysTick_Handler(void); //Прерывания от системного таймера
     void CMSIS_PC13_OUTPUT_Push_Pull_init(void); //Пример настройки ножки PC13 в режим Push-Pull 50 MHz
+	void CMSIS_GPIO_init(GPIO_TypeDef *GPIO, uint8_t GPIO_Pin, uint8_t Configuration_mode, uint8_t Type, uint8_t Speed); //Конфигурация GPIO
     void CMSIS_Blink_PC13(uint32_t ms); //Обычный blink
     void CMSIS_PA8_MCO_init(void); //Пример настройки ножки PA8 в выход тактирующего сигнала c MCO
     void CMSIS_RCC_AFIO_enable(void); //Включить тактирование для альтернативных функций
@@ -83,6 +108,8 @@ extern "C" {
     void FLASH_Page_write(uint32_t Adress, uint8_t *Data, uint16_t Size); //Запись страницы во FLASH
     void FLASH_Read_data(uint32_t Adress, uint8_t *Data, uint16_t Size); //Считывание данных с FLASH
     
+    void xPortSysTickHandler( void );
+
 #ifdef __cplusplus
 }
 #endif
